7b026a66a4fb3b737e00a9ae8ab5517fd2ac5611
Trevor Byington <tbyington@instructure.com>
2018-12-11 13:41:02 -0700

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_12_11_202902) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "resources", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
