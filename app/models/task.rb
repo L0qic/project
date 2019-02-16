2643c1f8eeec56b350f4266d15499cf601d80f68
Trevor Byington <tbyington@instructure.com>
2019-02-15 19:15:41 -0700

    Add devise and some more css tweeks

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,8 +14,24 @@ class Task < ApplicationRecord
       end
     end
     else
-      qa_day = true if (self.start.beginning_of_day <= date) && (self.end.end_of_day >= date)
+      qa_day = true if self.start && (self.start.beginning_of_day <= date) && (self.end.end_of_day >= date)
     return qa_day
+  end
+
+  def active_milestone(date)
+    ms = false
+    if milestones
+      milestones.each do |milestone|
+        if (is_qa_day(date) && date > milestone.dev_end_date && date <= milestone.dev_end_date+milestone.qa_days)
+          ms = milestone.desc
+          return ms
+        end
+      end
+    end
+    if !ms
+      ms = name
+    end
+    return ms
   end
 
   def all_days_past
@@ -26,7 +42,7 @@ class Task < ApplicationRecord
         past = false
       end
     end
-    if self.end.end_of_day >= t
+    if self.end && self.end.end_of_day >= t
       past = false
     end
     return past
