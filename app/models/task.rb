687432fbd8e7dd4d8e3a880bd9bb4a1c14bf0cce
Trevor Byington <tbyington@instructure.com>
2019-02-15 17:49:40 -0700

    Add support for skipping weekends

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,16 +3,33 @@ class Task < ApplicationRecord
   has_many :milestones
 
   def is_qa_day (date)
-    puts "Checking #{date}"
     qa_day = false
     if milestones
       milestones.each do |milestone|
+        if (date.sunday? or date.saturday?)
+          milestone.qa_days += 1
+          return qa_day
+        end
         qa_day = true if (date > milestone.dev_end_date) && (date <= milestone.dev_end_date + milestone.qa_days.days)
       end
     end
     else
       qa_day = true if (self.start.beginning_of_day <= date) && (self.end.end_of_day >= date)
     return qa_day
+  end
+
+  def all_days_past
+    t = Time.new
+    past = true
+    milestones.each do |m|
+      if (m.dev_end_date + m.qa_days) > t
+        past = false
+      end
+    end
+    if self.end.end_of_day >= t
+      past = false
+    end
+    return past
   end
 
 end
