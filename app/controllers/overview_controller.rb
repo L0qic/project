22abacc3ea2d3ff2d25623d51e6dc246b1279bab
Trevor Byington <tbyington@instructure.com>
2019-03-07 10:30:26 -0700

    had to do some tweeking

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -5,3 +5,10 @@ class OverviewController < ApplicationController
     @cal = Time.new
   end
 end
+
+private
+  def date_cell_html(number, color, text="")
+    html = "<table><tr><td class='number'><span class='date_display'>#{number}</span></td></tr>"
+    html += "<tr><td bgcolor='#{color}' class='date_cell'><span class='tooltip'>#{text}</span></td></tr>"
+    html += "</table>"
+  end
